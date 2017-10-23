3dabeacc95f00b330a25c44c20f87b436dda1433
Emily Sharp <emilysharp@Emilys-MacBook-Air.local>
2017-10-23 11:21:29 -0500

    added front-end files

---
+++ client/controllers.js
@@ -0,0 +1,28 @@
+angular.module('store.controllers', ['ngRoute'])
+
+.controller('WelcomeController', [{
+
+}])
+
+.controller('ApparelController', [{
+
+}])
+
+.controller('MiscController', [{
+	
+}])
+
+.controller('SingleController', [{
+	
+}])
+
+.controller('ContactController', [{
+	
+}])
+
+.controller('CartController', [{
+	
+}])
+
+
+
