675c35ab58914a57440345c712990d1000d8d6c0
Aparajita Chakrabarty <aparajitachakrabarty@Aparajitas-MacBook-Air.local>
2017-10-24 14:37:13 -0500

    Fixing backend calls

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,60 +1,60 @@
-import * as mysql from 'mysql';
+import * as mysql from "mysql";
 
 export let pool = mysql.createPool({
-    connectionLimit: 10,
-    host: 'us-cdbr-iron-east-05.cleardb.net',
-    user: 'b5a35611db66ad',
-    password: '6ce8f5d4',
-    database: 'CovalenceStore'
+  connectionLimit: 10,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE
 });
 
-function callProcedure(procedureName: string, args: Array<any> = []): Promise<Array<Array<any>>> {
-    return new Promise((resolve, reject)=> {
-        pool.getConnection((err, connection)=> {
-            if(err) {
-                reject(err);
+function callProcedure(
+  procedureName: string,
+  args: Array<any> = []
+): Promise<Array<Array<any>>> {
+  return new Promise((resolve, reject) => {
+    pool.getConnection((err, connection) => {
+      if (err) {
+        reject(err);
+      } else {
+        let placeholders = "";
+        if (args && args.length > 0) {
+          for (let i = 0; i < args.length; i++) {
+            if (i === args.length) {
+              placeholders += "?";
             } else {
-                let placeholders = '';
-                if ( args && args.length > 0) {
-                    for (let i = 0; i < args.length; i++) {
-                        if (i === args.length) {
-                            placeholders += '?';
-                        } else {
-                            placeholders += '?,';
-                        }
-                    }
-                }
-                let callString = 'CALL' + procedureName + '(' + placeholders + ');';
-                connection.query( callString, args, (err, resultsets)=> {
-                    connection.release();
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(resultsets);
-                    }
-                });
+              placeholders += "?,";
             }
+          }
+        }
+        let callString = "CALL " + procedureName + "(" + placeholders + ");";
+        connection.query(callString, args, (err, resultsets) => {
+          connection.release();
+          if (err) {
+            reject(err);
+          } else {
+            resolve(resultsets);
+          }
         });
+      }
     });
+  });
 }
 
-export function rows( procedureName: string, args: Array<any> = []){
-    return callProcedure(procedureName, args)
-    .then((resultsets)=>{
-        return resultsets[0];
-    });
+export function rows(procedureName: string, args: Array<any> = []) {
+  return callProcedure(procedureName, args).then(resultsets => {
+    return resultsets[0];
+  });
 }
 
-export function row( procedureName: string, args: Array<any> = []){
-    return callProcedure(procedureName, args)
-    .then((resultsets)=>{
-        return resultsets[0][0];
-    });
+export function row(procedureName: string, args: Array<any> = []) {
+  return callProcedure(procedureName, args).then(resultsets => {
+    return resultsets[0][0];
+  });
 }
 
-export function empty( procedureName: string, args: Array<any> = []){
-    return callProcedure(procedureName, args)
-    .then(()=>{
-        return;
-    })
+export function empty(procedureName: string, args: Array<any> = []) {
+  return callProcedure(procedureName, args).then(() => {
+    return;
+  });
 }
--- server/controllers/products.ctrl.ts
+++ server/controllers/products.ctrl.ts
@@ -1,16 +1,16 @@
 import * as express from 'express';
-import * as products from '../procedures/products.proc';
+import * as procedures from '../procedures/products.proc';
 
 let router = express.Router();
 
-// router.get('/', (req, res) => {
-//     all()
-//     .then((products) => {
-//         res.send(products);
-//     }).catch((err) => {
-//         console.log(err);
-//         res.sendStatus(500);
-//     });
-// });
+router.get('/', (req, res) => {
+    procedures.all()
+    .then((products) => {
+        res.send(products);
+    }).catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    });
+});
 
 export default router;
