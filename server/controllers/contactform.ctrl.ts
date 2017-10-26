958166fe47fe4d4cc2085aecd3f33cebb8e28b17
Aparajita Chakrabarty <aparajitachakrabarty@Aparajitas-MacBook-Air.local>
2017-10-26 10:25:12 -0500

    added cart changes

--- server/controllers/contactform.ctrl.ts
+++ server/controllers/contactform.ctrl.ts
@@ -4,14 +4,14 @@ import { sendEmail } from '../services/email.svc';
 const router = Router();
 
 router.post('/', (req, res) => {
-    sendEmail('jeff@covalence.io', req.body.from, 'You have a new contact form submission', req.body.mes)
+    sendEmail('stinson.lyon@gmail.com', req.body.from, 'You have a new contact form submission', req.body.message)
     .then((response) => {
         res.sendStatus(201);
     })
     .catch((err) => {
         console.log(err);
         res.sendStatus(500);
-    })
-})
+    });
+});
 
 export default router;
